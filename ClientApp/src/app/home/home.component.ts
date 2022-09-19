7ab305132cbfcfc1837a1785835138fc31723a65
Roman Khananaev <rom95h@gmail.com>
2022-09-19 23:18:55 +0300

    End, all works

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -14,7 +14,7 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class HomeComponent {
   @ViewChild('iframe') iframe!: ElementRef;
   
-
+  data: any;
   view: any;
   x: any;
   y: any;
@@ -37,6 +37,9 @@ export class HomeComponent {
   roomObjToSearch: any;
 
   desc: any;
+  buildings = ["building1", "building3", "building4", "building6","building7"];
+  hidebuildings = ["1", "3", "6", "4", "7"];
+  buildingLayerId: any[] = [];
 
 
 
@@ -72,35 +75,43 @@ export class HomeComponent {
     this.http.get("assets/map/data/index/database.json").subscribe((data: any) => {
       let counter = 0;
       console.log("data: ", data);
+      this.data = data;
       for (let item of data.layers) {
+        console.log("layer: ", item);
         if (item.properties.type != 'dem') {
-          for (let item2 of item.data.blocks[0].features) {
-            //console.log("Item2: ", item2);
-            let x = item2.geom.centroids[0][0];
-            let y = item2.geom.centroids[0][1];
-            let id = item2.prop[0];
-            let room = item2.prop[1];
-            let desc = item2.prop[2];
-            let building = item2.prop[3];
-            let floor = item2.prop[4];
-            let type = item2.prop[5];
-            let direction = item2.prop[6];
-            let row = {
-              "x": x,
-              "y": y,
-              "id": id,
-              "room": room,
-              "desc": desc,
-              "building": building,
-              "floor": floor,
-              "type": type,
-              "direction": direction,
+          if (!this.buildings.includes(item.properties.name)) {
+            for (let item2 of item.data.blocks[0].features) {
+              //console.log("Item2: ", item2);
+              let x = item2.geom.centroids[0][0];
+              let y = item2.geom.centroids[0][1];
+              let id = item2.prop[0];
+              let room = item2.prop[1];
+              let desc = item2.prop[2];
+              let building = item2.prop[3];
+              if (building == null) {
+                console.log("error: ", item);
+              }
+              let floor = item2.prop[4];
+              let type = item2.prop[5];
+              let direction = item2.prop[6];
+              let row = {
+                "x": x,
+                "y": y,
+                "id": id,
+                "room": room,
+                "desc": desc,
+                "building": building,
+                "floor": floor,
+                "type": type,
+                "direction": direction,
+              }
+              this.database.push(row);
+              counter++;
             }
-            this.database.push(row);
-            counter++;
+
+            //console.log("Item: ", item);
           }
-          //console.log("Item: ", item);
-          }  
+        }
       }
       //console.log("counter: ", counter);
       console.log("Database: ", this.database);
@@ -115,7 +126,7 @@ export class HomeComponent {
 
 
     
-    this.view = "https://localhost:44487/assets/map/index.html#cx=3871055.142&cy=3765013.485&cz=96.140&tx=3871022.477&ty=3765166.914&tz=0.000";
+    this.view = "https://localhost:44487/assets/map/index.html#cx=3870698.346&cy=3765004.293&cz=237.379&tx=3870931.904&ty=3765209.380&tz=2.000";
     
     
   }
@@ -148,6 +159,7 @@ export class HomeComponent {
     //console.log(this.selectedType);
     for (let item of this.database) {
       if (item.building == this.selectedBuilding && item.type == this.selectedType) {
+        console.log("item: ", item);
         this.databaseRoomNum.push(item.room);
       }
 
@@ -172,6 +184,23 @@ export class HomeComponent {
       }
 
     }
+  }
+
+
+  showAll() {
+    for (let layer of this.data.layers) {
+        this.iframe.nativeElement.contentWindow.app.scene.mapLayers[layer.id].visible = true;
+    }
+  }
+
+  hideBulding(building: string) {
+    this.showAll();
+    for (let layer of this.data.layers) {
+      if (layer.properties.name == building) {
+        this.iframe.nativeElement.contentWindow.app.scene.mapLayers[layer.id].visible = false;
+      }
+    }
+
   }
 
   search() {
@@ -206,6 +235,11 @@ export class HomeComponent {
       this.cz = 2 + this.factor;
 
     }
+    else if (this.roomObjToSearch.floor == "-1") {
+      this.z = -2 + this.factor;
+      this.cz = -2 + this.factor;
+      //console.log("Floor 2")
+    }
     else if (this.roomObjToSearch.floor == "2") {
       this.z = 5 + this.factor;
       this.cz = 5 + this.factor;
@@ -215,6 +249,21 @@ export class HomeComponent {
       this.z = 8 + this.factor;
       this.cz = 8 + this.factor;
       //console.log("Floor 3")
+    }
+    else if (this.roomObjToSearch.floor == "4") {
+      this.z = 11 + this.factor;
+      this.cz = 11 + this.factor;
+      //console.log("Floor 4")
+    }
+    else if (this.roomObjToSearch.floor == "5") {
+      this.z = 14 + this.factor;
+      this.cz = 14 + this.factor;
+      //console.log("Floor 5")
+    }
+    else if (this.roomObjToSearch.floor == "6") {
+      this.z = 17 + this.factor;
+      this.cz = 17 + this.factor;
+      //console.log("Floor 6")
     }
     else {
 
@@ -232,7 +281,13 @@ export class HomeComponent {
 
 
 
-    setTimeout(() => { this.click(); }, 500);
+    setTimeout(() => {
+      if (this.hidebuildings.includes(this.roomObjToSearch.building)) {
+        this.hideBulding("building" + this.roomObjToSearch.building);
+      }
+      
+      this.click();
+    }, 500);
 
   }
 
